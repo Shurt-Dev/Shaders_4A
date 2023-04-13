d7393942012b3708c494b151a81bf096e2e77a7d
ulysst <ulysstrachet@gmail.com>
2023-04-13 20:36:23 +0100

    Barrier

---
+++ Assets/Scripts/OpenBarrier.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenBarrier : MonoBehaviour
+{
+    [SerializeField] GameObject barrier;
+    private void OnTriggerEnter(Collider collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            barrier.SetActive(false);
+            gameObject.SetActive(false);
+        }
+       
+    }
+}
