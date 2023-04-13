fbf22d53532883fdbd505cf662c75c41ee563c1e
ulysst <ulysstrachet@gmail.com>
2023-04-13 20:24:38 +0100

    Mats

--- Assets/Scripts/BubbleContact.cs
+++ Assets/Scripts/BubbleContact.cs
@@ -10,7 +10,27 @@ public class BubbleContact : MonoBehaviour
     {
         if(Collision.gameObject.tag == "ITrigger")
         {
-            InvincibilityBubble.SetActive(true);
+            StartCoroutine(TimerInvincibility());
         }
+
+        if (Collision.gameObject.tag == "HTrigger")
+        {
+            StartCoroutine(TimerHealing());
+        }
+    }
+
+   public IEnumerator TimerInvincibility()
+    {
+        float duration = 10f;
+        InvincibilityBubble.SetActive(true);       
+        yield return new WaitForSeconds(duration);
+        InvincibilityBubble.SetActive(false);
+    }
+    public IEnumerator TimerHealing()
+    {
+        float duration = 2f;
+        HealingBubble.SetActive(true);
+        yield return new WaitForSeconds(duration);
+        HealingBubble.SetActive(false);
     }
 }
